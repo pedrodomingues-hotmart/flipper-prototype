84ae6eb3ee3e74f5697fa0b0680576f078b4470f
Pedro Henrique Oliveira <pedro.domingues@hotmart.com>
2021-09-29 16:41:14 -0300

    feat: update flipper initializer to a better form

--- apps/simple-api/config/initializers/flipper.rb
+++ apps/simple-api/config/initializers/flipper.rb
@@ -1,10 +1,7 @@
 require 'flipper/adapters/active_record'
 
 Flipper.configure do |config|
-  config.default do
-    adapter = Flipper::Adapters::ActiveRecord.new
-    Flipper.new(adapter)
-  end
+  config.adapter { Flipper::Adapters::ActiveRecord.new }
 end
 
 Flipper.register(:admins) do |actor|
